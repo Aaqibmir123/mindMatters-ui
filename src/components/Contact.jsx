d3d1bb28fd62de128807a4ce11cf4777695dde72
Aaqibmir123 <miraaqib514@gmail.com>
2026-07-26 10:05:14 +0530

    mapUpdated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,17 +59,17 @@ export default function Contact() {
           </div>
 
           {/* Map */}
-          <div className="lg:col-span-7">
-            <div className="rounded-xl overflow-hidden border border-slate-200 shadow-sm h-96">
-              <iframe
-                title="Clinic Location"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.983226487141!2d77.216721!3d28.63042!2m3!1f0!0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjhCsDM3JzA5LjUiTiA3N8KwMTMnMDAuMiJF!5e0!3m2!1sen!2sin!4v1620000000000!5m2!1sen!2sin"
-                className="w-full h-full border-0"
-                loading="lazy"
-                allowFullScreen
-              ></iframe>
-            </div>
-          </div>
+        <div className="lg:col-span-7">
+  <div className="rounded-xl overflow-hidden border border-slate-200 shadow-sm h-96">
+  <iframe
+  title="Clinic Location"
+  src="https://www.google.com/maps?q=Firdous+Abad+Chowk+near+J%26K+Bank+ATM+Tengpora+Srinagar&output=embed"
+  className="w-full h-full border-0"
+  loading="lazy"
+  allowFullScreen
+/>
+  </div>
+</div>
         </div>
       </div>
     </section>
